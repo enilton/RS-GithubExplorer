3daef80c9f11c8d317f90ac74e9427f0cba48e9e
Enilton Rastele <eniltjr1@gmail.com>
2020-07-19 00:43:33 -0300

    feature:dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,11 @@ import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
 
-const App: React.FC = () => (
-  <>
-  <GlobalStyle>
+const App: React.FC = () => (  
     <BrowserRouter>
       <Routes />
+      <GlobalStyle />
     </BrowserRouter>
-  </GlobalStyle>
-  </>
 );
 
 export default App;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,88 @@
-import React from 'react';
+import React, { useState, FormEvent, useEffect } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/rocketseat_logo.png';
 
-import { Title } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
+
+interface Repository {
+    full_name: string;
+    description: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    }
+}
 
 const Dashboard: React.FC = () => {
+    const [newRepo, setNewRepo] = useState('');
+    const [inputError, setInputError] = useState('');
+    const [repositories, setRepositories] = useState<Repository[]>(() => {
+        const storagedRepositories = localStorage.getItem('@githubExplorer:repositories');
+        if (storagedRepositories) {
+            return JSON.parse (storagedRepositories);
+        } else {
+            return [];
+        }
+    });
+
+    useEffect(() => {
+        localStorage.setItem('@githubExplorer:repositories', JSON.stringify(repositories));
+    }, [repositories])
+
+    async function handleAddRepository(event: FormEvent<HTMLFormElement>):Promise<void>{
+        event.preventDefault();
+        if(!newRepo) {
+            setInputError('Digite o autor/nome do repositorio');
+            return;
+        }
+       
+        try {
+            const response = await api.get<Repository>(`repos/${newRepo}`);
+            setRepositories([...repositories, response.data]);
+            setInputError('');
+            setNewRepo('');
+        }catch (error){
+            setInputError('Erro na busca por esse repositório');
+        }
+    }
+
     return (
         <>
             <img src={logoImg} alt="Github Explorer" />
             <Title>Explore repositórios no Github</Title>
+
+            <Form 
+                hasError={!!inputError}
+                onSubmit={handleAddRepository}>
+                <input 
+                    value={newRepo}
+                    onChange={(e):void => setNewRepo(e.target.value)}
+                    placeholder="Digite o nome do repositório"></input>
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            { inputError  &&             
+                <Error>{inputError}</Error>
+            }
+
+            <Repositories>
+                {repositories.map(repository => (
+                    <a key={repository.full_name} href="teste">
+                        <img 
+                            src={repository.owner.avatar_url} 
+                            alt={repository.owner.login}/>
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
+                        <FiChevronRight size={20}/>
+                    </a>           
+                 ))}
+                           
+            </Repositories>
         </>
     )
 }
